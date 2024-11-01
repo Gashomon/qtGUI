9873ba64f80a38d61589472a96aec6c0f15f444c
GitAshoMon <monteagudoace@email.com>
2024-11-01 20:02:01 +0800

    pass by assignment try

--- try.py
+++ try.py
@@ -1,18 +1,13 @@
 import MyGUI
 import sys
 n = 10
+blah = ''
 m = [10]
 hello = MyGUI.GUI
 hello.widget.show()
 
 def func1(p):
-    # hello.widget.setCurrentIndex(1)
     out = p + 30
-    # hello
-    # while (hello.widget.currentIndex() == 1):
-    #     if (hello.widget.currentIndex() == 2):
-    # #         return out
-    # if (hello.widget.currentIndex() == 2):
     return out
 def func2():
     hello.status.label_2.setText(str(n))
@@ -23,22 +18,32 @@ def func3(p):
 def func4():
     hello.status.label_2.setText(str(m[0]))
 
+def func5(nonsense, p):
+    out = p + 30    
+    lol = nonsense
+    return out
+
+
 hello.control.pushButton_2.clicked.connect(lambda: func1(n))
 hello.control.pushButton_5.clicked.connect(lambda: func2())
-hello.control.pushButton_2.clicked.connect(lambda: func3(m))
-hello.control.pushButton_5.clicked.connect(lambda: func4())
+# hello.control.pushButton_2.clicked.connect(lambda: func3(m))
+# hello.control.pushButton_5.clicked.connect(lambda: func4())
+hello.control.pushButton_2.clicked.connect(lambda: func5(n))
 
 hello.control.pushButton_3.clicked.connect(lambda: func1(n))
 hello.control.pushButton_6.clicked.connect(lambda: func2())
-hello.control.pushButton_3.clicked.connect(lambda: func3(m))
-hello.control.pushButton_6.clicked.connect(lambda: func4())
+# hello.control.pushButton_3.clicked.connect(lambda: func3(m))
+# hello.control.pushButton_6.clicked.connect(lambda: func4())
+hello.control.pushButton_3.clicked.connect(lambda: func5(n))
 
 hello.control.pushButton_4.clicked.connect(lambda: func1(n))
 hello.control.pushButton_7.clicked.connect(lambda: func2())
-hello.control.pushButton_4.clicked.connect(lambda: func3(m))
-hello.control.pushButton_7.clicked.connect(lambda: func4())
+# hello.control.pushButton_4.clicked.connect(lambda: func3(m))
+# hello.control.pushButton_7.clicked.connect(lambda: func4())
+hello.control.pushButton_4.clicked.connect(lambda: func5(n))
 
 print(n)
-n = func1(n)
+# n = func1(n)
+func5(blah, n)
 print(n)
 sys.exit(hello.app.exec())
